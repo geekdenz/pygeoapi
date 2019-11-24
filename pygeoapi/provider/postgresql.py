26cfd878e7675726492ebef5ed1dde9f54ed2907
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2019-11-24 18:17:10 +1300

    Add support for arbitrary data projections in example client display (Leaflet) #304.

--- pygeoapi/provider/postgresql.py
+++ pygeoapi/provider/postgresql.py
@@ -263,6 +263,9 @@ class PostgreSQLProvider(BaseProvider):
                 feature_collection['features'].append(
                     self.__response_feature(rd))
 
+            cursor.execute(SQL('SELECT ST_SRID({}) AS srid FROM {} LIMIT 1').format(Identifier(self.geom), Identifier(self.table)))
+            srid_result = cursor.fetchall()
+            feature_collection['crs:epsg'] = srid_result[0]['srid']
             return feature_collection
 
     def get(self, identifier):
